586fae4644b8117138a959d9a5b1b1f8b50cf79a
Lamin Sanneh <lamin.evra@gmail.com>
2024-04-15 20:04:59 +0000

    Add homepage coponent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Typography, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const Home = () => {
   return (
     <div>
-      <Typography variant="h4">Welcome to Money Transfer Service</Typography>
-      <Button variant="contained" color="primary">Make Transfer</Button>
+      <Typography variant="h3" align="center" gutterBottom>
+        Welcome to the Money Transfer Service
+      </Typography>
+      <Typography variant="body1" align="center">
+        This is a simple and secure platform for transferring money internationally.
+      </Typography>
+      <Typography variant="body1" align="center">
+        Please <a href="/login">login</a> or <a href="/register">register</a> to get started.
+      </Typography>
     </div>
   );
-}
+};
 
 export default Home;
